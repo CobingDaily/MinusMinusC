c95372187093ad717575895dd39c3f796f92f146
CobingDaily <69477634+CobingDaily@users.noreply.github.com>
2025-12-20 02:48:11 +0100

    new

--- main.c
+++ main.c
@@ -2,14 +2,32 @@
 #include <stdlib.h>
 #include <assert.h>
 
+// #define new(value) ({ \
+//     __typeof__(value)* object_name; \
+//     malloc_assert(&object_name); \
+// })
+
+void* new_int(int value) {
+    int* address = malloc(sizeof(*address));
+    assert(address != 0);
+
+    *address = value;
+    return address;
+}
+
+void* new_float(float value) {
+    float* address = malloc(sizeof(*address));
+    assert(address != 0);
+
+    *address = value;
+    return address;
+}
+
 int main(void) {
-    printf("Hello, World\n");
-
-    int* number = malloc(sizeof(int));
-    assert(number != 0);
-
-    *number = 4;
+    int* number = new_int(4);
+    float* pi = new_float(3.14f);
 
     printf("*number = %d\n", *number);
+    printf("*pi = %f\n", *pi);
 }
 
