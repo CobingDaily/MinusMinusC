65edb5665a143463c8bc6d0170a3d600ed588306
CobingDaily <69477634+CobingDaily@users.noreply.github.com>
2025-12-20 01:31:50 +0100

    test

--- main.c
+++ main.c
@@ -1,5 +1,15 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <assert.h>
 
 int main(void) {
     printf("Hello, World\n");
+
+    int* number = malloc(sizeof(int));
+    assert(number != 0);
+
+    *number = 4;
+
+    printf("*number = %d\n", *number);
 }
+
